1fb3bb49998077583e7978e26e137a534dc39a83
engDaniloOS <36059157+engDaniloOS@users.noreply.github.com>
2024-03-04 21:26:48 -0300

    Update main.tf

--- iCapital/AWS/main.tf
+++ iCapital/AWS/main.tf
@@ -1,3 +1,15 @@
 provider "aws"{
     region = "us-east-1"
 }
+
+terraform {
+    backend "remote" {
+        # The name of your Terraform Cloud organization.
+        organization = "testen"
+
+        # The name of the Terraform Cloud workspace to store Terraform state files in.
+        workspaces {
+            name = "teste"
+        }
+    }
+}
