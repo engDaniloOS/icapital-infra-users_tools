20a971d32cc5ee98cb918990b467b22a138d4cea
engDaniloOS <36059157+engDaniloOS@users.noreply.github.com>
2024-03-17 20:03:55 -0300

    Update ecs_service.tf

--- iCapital/AWS/ecs_service.tf
+++ iCapital/AWS/ecs_service.tf
@@ -75,7 +75,7 @@ resource "aws_ecs_task_definition" "task_definition" {
         {
           containerPort = 8080,
           hostPort      = 8080,
-          protocol      = "tcp",
+          protocol      = "HTTP",
         }
       ],
       logConfiguration = {
@@ -110,7 +110,7 @@ resource "aws_lb_target_group" "target_group" {
   protocol = "HTTP"
   target_type = "ip"
   vpc_id   = "vpc-0b3d7c2f0a9a183c3"
-  
+
   health_check {
     path                = "/actuator/health"
     port                = "traffic-port"
