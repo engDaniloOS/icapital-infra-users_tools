a463c1455a1e195107b8801f26fb261a593314b8
engDaniloOS <36059157+engDaniloOS@users.noreply.github.com>
2024-03-21 00:35:36 -0300

    Update iam.tf

--- iCapital/AWS/iam.tf
+++ iCapital/AWS/iam.tf
@@ -65,5 +65,15 @@ resource "aws_iam_policy" "ecr_access_policy" {
 
 resource "aws_iam_role_policy_attachment" "ecs_task_policy_attachment" {
   role       = aws_iam_role.icapital_task_role.name
-  policy_arn = [aws_iam_policy.ecs_task_policy.arn, data.aws_iam_policy.service_role_policy.arn, aws_iam_policy.ecr_access_policy.arn]
+  policy_arn = aws_iam_policy.ecs_task_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "service_task_policy_attachment" {
+  role       = aws_iam_role.icapital_task_role.name
+  policy_arn = aws_iam_policy.service_role_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_task_policy_attachment" {
+  role       = aws_iam_role.icapital_task_role.name
+  policy_arn = aws_iam_policy.ecr_access_policy.arn
 }
