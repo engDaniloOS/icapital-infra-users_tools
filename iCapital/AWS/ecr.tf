208a280ce8a50d8da1678f3d7298cdd799b31fac
engDaniloOS <36059157+engDaniloOS@users.noreply.github.com>
2024-03-24 17:52:43 -0300

    criacao ecr

--- iCapital/AWS/ecr.tf
+++ iCapital/AWS/ecr.tf
@@ -1,28 +1,28 @@
-# resource "aws_ecr_repository" "icapital_users_tools" {
-#   name                 = "icapital-users-tools"
-# }
+resource "aws_ecr_repository" "icapital_users_tools" {
+  name                 = "icapital-users-tools"
+}
 
-# # Política de permissão para manter o registro privado
-# resource "aws_ecr_repository_policy" "icapital_users_tools_policy" {
-#   repository = aws_ecr_repository.icapital_users_tools.name
+# Política de permissão para manter o registro privado
+resource "aws_ecr_repository_policy" "icapital_users_tools_policy" {
+  repository = aws_ecr_repository.icapital_users_tools.name
 
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Sid       = "AllowPushPull",
-#         Effect    = "Allow",
-#         Principal = "*",
-#         Action    = [
-#           "ecr:GetDownloadUrlForLayer",
-#           "ecr:BatchGetImage",
-#           "ecr:BatchCheckLayerAvailability",
-#           "ecr:PutImage",
-#           "ecr:InitiateLayerUpload",
-#           "ecr:UploadLayerPart",
-#           "ecr:CompleteLayerUpload"
-#         ]
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "AllowPushPull",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+      }
+    ]
+  })
+}
